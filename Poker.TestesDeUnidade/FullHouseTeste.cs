1c5d00b71e7d6517dab45b7867ec390127e1996f
Marlom Souza <marlom1012@gmail.com>
2019-05-13 21:25:35 -0300

    full house teste

--- Poker.TestesDeUnidade/FullHouseTeste.cs
+++ Poker.TestesDeUnidade/FullHouseTeste.cs
@@ -1,7 +1,33 @@
+using Poker.Dominio.Jogadas;
+using Xunit;
+
 namespace Poker.TestesDeUnidade
 {
     public class FullHouseTeste
     {
-        
+        [Theory]
+        [InlineData(new[] { 1, 1, 1, 5, 5 })]
+        [InlineData(new[] { 7, 2, 7, 2, 7 })]
+        [InlineData(new[] { 3, 3, 4, 4, 4 })]
+        public void Deve_ser_uma_jogada_valida(int[] mao)
+        {
+            //When
+            var jogada = new FullHouse(mao);
+            //Then
+            Assert.True(jogada.EhValida());
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 5, 6 })]
+        [InlineData(new[] { 1, 1, 1, 5, 6 })]
+        [InlineData(new[] { 2, 2, 2, 2, 7 })]
+        [InlineData(new[] { 5, 5, 3, 4, 4 })]
+        public void Nao_deve_ser_uma_jogada_valida(int[] mao)
+        {
+            //When
+            var jogada = new FullHouse(mao);
+            //Then
+            Assert.False(jogada.EhValida());
+        }
     }
 }
